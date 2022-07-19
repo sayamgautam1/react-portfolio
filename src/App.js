6a7bd60ae0359eb94892420ccdefb2f529236ac4
Sayam Gautam <sayamgautam1@gmail.com>
2022-07-19 20:41:36 +0930

    Add animation when the page loads

--- src/App.js
+++ src/App.js
@@ -11,12 +11,36 @@ const HomeContainer = styled.div`
 const Blue = styled.div`
   background-color: #ecf3f4;
   height: 100%;
+  animation: slideFromRight 1s forwards;
+  @keyframes slideFromRight {
+    0% {
+      transform: translate(2000px);
+      opacity: 0;
+    }
+    100% {
+      transform: translate(0);
+      opacity: 1;
+    }
+  }
   & h1 {
     position: relative;
     font-size: 200px;
     line-height: 91.45%;
     padding: 250px 0 0 300px;
-    letter-spacing: 0px;
+    animation: slideFromLeft 1s forwards;
+    animation-delay: 0.5s;
+    pointer-events: none;
+    opacity: 0;
+  }
+  @keyframes slideFromLeft {
+    0% {
+      transform: translate(-1000px);
+      opacity: 0;
+    }
+    100% {
+      transform: translate(0px);
+      opacity: 1;
+    }
   }
 `;
 const Cutout = styled.div`
@@ -25,6 +49,17 @@ const Cutout = styled.div`
   height: 0;
   border-top: 1100px solid white;
   border-right: 700px solid transparent;
+  animation: ShiftEdge 1s forwards;
+  @keyframes ShiftEdge {
+    0% {
+      border-top: 2400px solid white;
+      border-right: 300px solid transparent;
+    }
+    100% {
+      border-top: 1100px solid white;
+      border-right: 700px solid transparent;
+    }
+  }
 `;
 
 function App() {
