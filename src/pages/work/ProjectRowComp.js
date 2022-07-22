4247b357ded1cd00d60e41328cb3fa1380b163a7
Sayam Gautam <sayamgautam1@gmail.com>
2022-07-22 21:19:59 +0930

    add project desp, images and link inside project row comp, remove a tag texzt decoration

--- src/pages/work/ProjectRowComp.js
+++ src/pages/work/ProjectRowComp.js
@@ -3,6 +3,7 @@ import { breakpoints } from "../../Media";
 import Heading from "../../components/Heading";
 import Paragraph from "../../components/Paragraph";
 import MediaAppThumbnail from "./moviesearchapp.png";
+import MyButcher from "./mybutcher.png";
 
 const ProjectContainer = styled.div`
   display: flex;
@@ -17,17 +18,45 @@ const ProjectContainer = styled.div`
   }
 `;
 
+const ProjectLink = styled(Paragraph)`
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 163.19%;
+  display: inline;
+  color: black;
+  letter-spacing: 0.03em;
+  text-transform: uppercase;
+  border-bottom: 2px solid transparent;
+  transition: border-bottom 0.3s;
+  :hover {
+    border-bottom: 2px solid black;
+  }
+  span {
+    font-size: 20px;
+  }
+`;
+
 const ProjectImage = styled.div`
   width: 100%;
   height: 300px;
   background-color: gray;
   border-radius: 4px;
+  transition-duration: 0.3s;
 `;
 
 const Project = styled.div`
   width: 100%;
   height: 450px;
   margin-bottom: 90px;
+  transition-duration: 0.3s;
+  :hover {
+    ${ProjectImage} {
+      transform: scale(1.03);
+    }
+    ${ProjectLink} {
+      border-bottom: 2px solid black;
+    }
+  }
   :nth-child(1) {
     margin-right: 0;
     @media (min-width: ${breakpoints.mobileMax}) {
@@ -42,19 +71,21 @@ const Project = styled.div`
     ${ProjectImage} {
       background-image: url(${MediaAppThumbnail});
       background-size: cover;
+      background-position-x: center;
     }
   }
   :nth-child(2) {
     ${ProjectImage} {
-      background-image: url(${MediaAppThumbnail});
+      background-image: url(${MyButcher});
       background-size: cover;
+      background-position-x: center;
     }
   }
 `;
-
 const SubHeading = styled(Heading)`
   font-size: 30px;
   margin-bottom: 0;
+  margin-top: 10px;
 `;
 
 const ProjectParagraph = styled(Paragraph)`
@@ -66,58 +97,58 @@ const ProjectParagraph = styled(Paragraph)`
   }
 `;
 
-const ProjectLink = styled(Paragraph)`
-  font-weight: 700;
-  font-size: 14px;
-  line-height: 163.19%;
-  display: inline;
-  color: black;
-  letter-spacing: 0.03em;
-  text-transform: uppercase;
-  border-bottom: 2px solid transparent;
-  transition: border-bottom 0.2s;
-  :hover {
-    border-bottom: 2px solid black;
-  }
-  span {
-    font-size: 20px;
-  }
-`;
-
 const ProjectRow = () => {
   return (
     <ProjectContainer>
       <Project>
-        <a href="#" target={"_blank"}>
+        <a
+          href="https://nolie88.github.io/appvengers-assemble/"
+          target={"_blank"}
+        >
           <ProjectImage />
         </a>
-        <a href="#" target={"_blank"}>
-          <SubHeading>Forecast</SubHeading>
+        <a
+          href="https://nolie88.github.io/appvengers-assemble/"
+          target={"_blank"}
+        >
+          <SubHeading>Media Search App</SubHeading>
         </a>
         <ProjectParagraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac
-          pretium magna.
+          A movie information and searching App using OMDB api and Youtube api.
         </ProjectParagraph>
-        <a href="#" target={"_blank"}>
+        <a
+          href="https://github.com/sayamgautam1/media-browser"
+          target={"_blank"}
+        >
           <ProjectLink>
-            Vist Site <span>&#8250;</span>
+            Vist Github <span>&#8250;</span>
           </ProjectLink>
         </a>
       </Project>
       <Project>
-        <a href="#">
+        <a
+          href="https://ordering-system-team4.herokuapp.com/"
+          target={"_blank"}
+        >
           <ProjectImage />
         </a>
-        <a href="#">
-          <SubHeading>Forecast</SubHeading>
+        <a
+          href="https://ordering-system-team4.herokuapp.com/"
+          target={"_blank"}
+        >
+          <SubHeading>E-commerce Meat Ordering App</SubHeading>
         </a>
         <ProjectParagraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac
-          pretium magna.
+          E-Commerce website, that allows you to make contracted orders from a
+          local butcher shop, review past orders and generate invoice of all
+          orders placed till date
         </ProjectParagraph>
-        <a href="#">
+        <a
+          href="https://github.com/sayamgautam1/E-Commerce_website"
+          target={"_blank"}
+        >
           <ProjectLink>
-            Vist Site <span>&#8250;</span>
+            Vist Github <span>&#8250;</span>
           </ProjectLink>
         </a>
       </Project>
