35ee508d84ff819ac67db7a4d7dcaa09bdd82d6d
Sayam Gautam <sayamgautam1@gmail.com>
2022-07-22 17:22:18 +0930

    similar layout for work component as about

--- src/pages/work/Work.js
+++ src/pages/work/Work.js
@@ -1,52 +1,54 @@
 import styled from "styled-components";
 import Hamburger from "../../components/hamburger/Hamburger";
+import IntroRow from "../about/IntroRow";
+import Square from "../../components/square/Square";
+import Footer from "../../components/footer/Footer";
 
 const Layout = styled.div`
-  width: 100vw;
-  height: 100vh;
-  padding: 60px;
+  display: flex;
+  min-height: 100vh;
+  max-width: 2000px;
+  margin: 0 auto;
+  overflow: hidden;
+  @media (min-width: 900px) {
+    width: 100vw;
+    height: 100vh;
+  }
 `;
 
 const Content = styled.div`
-  max-width: 2000px;
-  margin: 0 auto;
-  background-color: #f7f7f7;
+  margin: 30px;
   display: flex;
+  width: calc(100% - 90px);
+  flex: 1 0 auto;
   flex-wrap: wrap;
-  height: 100%;
   position: relative;
+  background-color: #f6f6f6;
+  overflow: scroll;
 `;
 
-const SquareTitle = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  padding: 0 30px 30px 5px;
-  background-color: white;
-  p {
-    margin: 0;
-    padding: 0;
-    font-weight: 800;
-    font-size: 150px;
-    line-height: 81.45%;
-    letter-spacing: -0.045em;
-    span {
-      color: orange;
-    }
-  }
+const AboutLanding = styled.div`
+  display: flex;
+  flex: 1 0 auto;
+  height: 100%;
+  flex-wrap: wrap;
 `;
+
+const RowSpacer = styled.div`
+  width: 100%;
+  padding: 80px 0;
+`;
+
 const Work = () => {
   return (
     <Layout>
       <Content>
-        <SquareTitle>
-          <p>
-            WO
-            <br />
-            RK <span>.</span>
-          </p>
-        </SquareTitle>
+        <AboutLanding>
+          <Square />
+          <IntroRow />
+        </AboutLanding>
         <Hamburger />
+        <Footer />
       </Content>
     </Layout>
   );
