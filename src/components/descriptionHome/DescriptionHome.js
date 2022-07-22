1cda5d984dc8e64e3c4f6a947aef82ceaa3f63ae
Sayam Gautam <sayamgautam1@gmail.com>
2022-07-22 18:16:43 +0930

    Add heading and paragraph componet inside DescriptionHome.js

--- src/components/descriptionHome/DescriptionHome.js
+++ src/components/descriptionHome/DescriptionHome.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components/macro";
+import Heading from "../Heading";
+import Paragraph from "../Paragraph";
 
 const Description = styled.div`
   padding: 100px 20px;
@@ -9,46 +11,19 @@ const Description = styled.div`
   display: flex;
   flex-direction: column;
   background-position: center center;
-  h1 {
-    text-align: center;
-    @media (min-width: 900px) {
-      text-align: left;
-      width: 100%;
-      font-family: "Lato", sans-serif;
-      font-style: normal;
-      font-weight: 800;
-      font-size: 40px;
-    }
-  }
-  p {
-    text-align: center;
-    margin: 30px auto;
-    width: 100%;
-    max-width: 500px;
-    font-family: "Lato", sans-serif;
-    font-weight: 400;
-    font-size: 15px;
-    line-height: 156.19%;
-    color: #5e5e5e;
-    letter-spacing: 0.03em;
-    @media (min-width: 900px) {
-      text-align: left;
-      margin: 30px 0;
-    }
-  }
 `;
 const DescriptionHome = () => {
   return (
     <Description>
-      <h1>Aspirant Full-Stack Developer</h1>
-      <p>
+      <Heading>Aspirant Full-Stack Developer</Heading>
+      <Paragraph>
         I’m a student at <strong>Adelaide University</strong>, a digital
         minimalist, below average football fan, a quick learner & passionate
         about technology.{" "}
         <strong>
           Feuled by black coffee and a drive to be better than I was yesterday.
         </strong>
-      </p>
+      </Paragraph>
     </Description>
   );
 };
