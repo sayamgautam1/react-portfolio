6d4c032d8825cc0033b837f5940b36068e3805a4
Sayam Gautam <sayamgautam1@gmail.com>
2022-07-22 18:52:38 +0930

    remove button componet and add button style only

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,22 +1,17 @@
-import styled from "styled-components";
+import styled from "styled-components/macro";
 
-import { Link } from "react-router-dom";
-
-const ButtonCont = styled.div`
-  max-width: 250px;
-`;
-const ButtonDiv = styled.p`
+const Button = styled.button`
   font-family: "Lato", sans-serif;
-  max-width: 90px;
   font-weight: 400;
   font-size: 15px;
   line-height: 156.19%;
   padding: 7px;
   color: #5e5e5e;
   letter-spacing: 0.03em;
+  border: none;
   border-bottom: 1px solid #5e5e5e;
   text-align: center;
-  transition: all 1s;
+  transition: all 0.2s;
   &:hover {
     border: none;
     border-bottom: 1px solid white;
@@ -24,14 +19,5 @@ const ButtonDiv = styled.p`
     box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.12);
   }
 `;
-const Button = (props) => {
-  return (
-    <ButtonCont>
-      <Link to={props.link}>
-        <ButtonDiv>{props.buttonText}</ButtonDiv>
-      </Link>
-    </ButtonCont>
-  );
-};
 
 export default Button;
